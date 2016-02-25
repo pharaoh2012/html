da88701151fd60e64089020a2085c3fc839c095f
pharaoh2012 <pharaoh168@gmail.com>
2016-02-25 16:16:41 +0800

    time

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@
 addEventListener("fetch", function(evt) {
 	evt.respondWith(new Promise(function(resolve, reject) {
 		setTimeout(function() {
-			resolve(new Response("Hi, Barret Lee"));
+			resolve(new Response("Hi, Barret Lee" + new Date()));
 		}, 1000);
 	}));
 });
