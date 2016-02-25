96d9f1abceb70ba590e81afa96a873a321b84829
pharaoh2012 <pharaoh168@gmail.com>
2016-02-25 16:16:00 +0800

    setTimeout

--- worker.js
+++ worker.js
@@ -1,7 +1,10 @@
 // workker.js
 addEventListener("fetch", function(evt) {
-	console.info(evt);
- 	evt.respondWith(new Response("Hi, Barret Lee"));
+	evt.respondWith(new Promise(function(resolve, reject) {
+		setTimeout(function() {
+			resolve(new Response("Hi, Barret Lee"));
+		}, 1000);
+	}));
 });
 
 console.info("workker.js");
